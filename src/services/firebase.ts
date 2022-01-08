0dd6ea533e15e3b312918ba9de61f4d9fd3a3b2c
Hugo Fortunato <hugo.fortunato@proenem.com.br>
2022-01-08 10:49:49 -0300

    first commit

---
+++ src/services/firebase.ts
@@ -0,0 +1,21 @@
+import firebase from 'firebase/compat/app';
+
+import 'firebase/compat/auth';
+import 'firebase/compat/database';
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDtIbmBPe7l9jod2A8EKXvipTasjwtZvD4",
+    authDomain: "letmeask-aulas-83f06.firebaseapp.com",
+    databaseURL: "https://letmeask-aulas-83f06-default-rtdb.firebaseio.com",
+    projectId: "letmeask-aulas-83f06",
+    storageBucket: "letmeask-aulas-83f06.appspot.com",
+    messagingSenderId: "389326031610",
+    appId: "1:389326031610:web:f63e345c3abda83daad743"
+  };
+  
+  // Initialize Firebase
+   firebase.initializeApp(firebaseConfig);
+
+   export const auth = firebase.auth()
+   export const database = firebase.database()
